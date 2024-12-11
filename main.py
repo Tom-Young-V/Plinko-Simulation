28b04af9f5d95d42314889dd6f454feec15104bc
Tom-Young-V <thyoungv@icloud.com>
2024-12-11 09:52:23 -0800

    Added pygame stuff

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-# Hello World
+from sys import exit
+import pygame
+
+pygame.init()
+
+infoObject = pygame.display.Info()
+screenLength = infoObject.current_w  # Screen width
+screenHeight = infoObject.current_h - 200  # Screen height
+screen = pygame.display.set_mode((screenLength, screenHeight))
+pygame.display.set_caption("Plinko")
+clock = pygame.time.Clock()
+fontName = "New York Times"
+font = pygame.font.SysFont(fontName, 40)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        
+    pygame.display.update()
+    clock.tick(60)
+
+
