054bf01cfb90617c422f90b99200c37071995409
Tom-Young-V <thyoungv@icloud.com>
2024-12-11 10:48:43 -0800

    Co-authored-by: lryvk <lryvk@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ pygame.init()
 
 infoObject = pygame.display.Info()
 screenLength = infoObject.current_w  # Screen width
-screenHeight = infoObject.current_h - 200  # Screen height
+screenHeight = infoObject.current_h - 120  # Screen height
 screen = pygame.display.set_mode((screenLength, screenHeight))
 pygame.display.set_caption("Plinko")
 clock = pygame.time.Clock()
-fontName = "New York Times"
+fontName = "Plinko"
 font = pygame.font.SysFont(fontName, 40)
+
+
+white = (255,255,255)
+black = (0,0,0)
+yellow = (255,255,0)
+red = (255,0,0)
+orange = (255,165,0)
 
 
 while True:
@@ -23,3 +30,40 @@ while True:
     clock.tick(60)
 
 
+
+
+
+
+# Bins section
+
+num_bins = 10
+bin_width = screenLength // num_bins
+bin_height = 15
+bin_colors = []
+
+for i in range(num_bins):
+    if i < 2 or i >= 8:
+        bin_colors.append(red)
+    elif i == 2 or i == 3 or i == 7 or i == 8:
+        bin_colors.append(orange)
+    else:
+        bin_colors.append(yellow)
+
+
+for i in range(num_bins):
+    pygame.draw.rect(
+        screen,
+        bin_colors[i],
+        (i * bin_width, screenHeight - bin_height, bin_width, bin_height)
+    )
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+
+    pygame.display.flip()  
+
+pygame.quit()
---
+++ peg.py
@@ -0,0 +1,14 @@
+
+
+pegRadius = 5
+pegColor = (171, 21, 123)
+rows = 8
+spacing = (10,10)
+
+class Peg:
+    def __init__(self, location):
+        self.location = location
+
+    def draw(self):
+        pygame.draw.rect
+    
