bac11752c39e2bff2ecc5bd277c27cc029b96b05
Tom-Young-V <thyoungv@icloud.com>
2025-01-13 09:36:51 -0800

    Co-authored-by: lryvk <lryvk@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-
 from sys import exit
 import pygame
 import random
+from math import floor
 
 pygame.init()
 
 displayObject = pygame.display.Info()
 screenLength = displayObject.current_w  # Screen width
-screenHeight = displayObject.current_h - 120  # Screen height
+screenHeight = displayObject.current_h - 100  # Screen height
 screen = pygame.display.set_mode((screenLength, screenHeight))
 pygame.display.set_caption("Plinko")
 clock = pygame.time.Clock()
@@ -25,15 +25,29 @@ pegSpacingX = 50
 pegSpacingY = 30
 
 ballRadius = 5
+
+smallFont = pygame.font.SysFont(fontName, 20)
+
 class Bin:
-    def __init__(self, position, color):
+    def __init__(self, position, color, multiplier):
         self.position = position
         self.color = color
-        #self.multiplier = multiplier
+        self.multiplier = multiplier
         self.rect = pygame.Rect(position[0], position[1], binWidth, binHeight)
 
     def draw(self):
         pygame.draw.rect(screen, self.color, self.rect, border_radius=corner_radius)
+     
+        if self.multiplier:  
+            multiplierText = f"x{int(self.multiplier)}"
+        else:
+            multiplierText = f"x{self.multiplier:.1f}"  
+
+       
+        multiplier_surface = smallFont.render(multiplierText, True, (255, 255, 255))
+        text_rect = multiplier_surface.get_rect(center=self.rect.center)
+        screen.blit(multiplier_surface, text_rect)
+
 
 class Peg:
     def __init__(self, position, color):
@@ -47,7 +61,7 @@ class Ball:
     def __init__(self, position):
         self.position = position
         self.color = (random.randint(128, 255), random.randint(128, 255), random.randint(128, 255))
-        self.velocity = (0, random.randint(-10, 10))
+        self.velocity = (random.randint(-3, 3), 0)
         self.acceleration = -9.8
         self.gravity = 0.1
 
@@ -56,37 +70,66 @@ class Ball:
         self.position = (self.position[0] + self.velocity[0], self.position[1] + self.velocity[1])
 
     def draw(self):
-        # self.update()
+        self.update()
         pygame.draw.circle(screen, self.color, self.position, ballRadius)
 
+
+class ClickableArea:
+    def __init__(self):
+        self.size = (200, 50)
+        self.position = (screenLength / 2 - self.size[0] // 2, 50)
+        self.color = (70, 74, 74)
+        self.rect = pygame.Rect(self.position[0], self.position[1], self.size[0], self.size[1])
+
+    def intersectMouse(self):
+        return self.rect.collidepoint(pygame.mouse.get_pos())
+
+    def draw(self):
+        pygame.draw.rect(screen, self.color, self.rect, border_radius = corner_radius)
 
 class Board:
     def __init__(self):
         self.createBins()
         self.createPegs()
-        
         self.balls = []
+
+        self.clickableArea = ClickableArea()
 
     def createBins(self):
         self.bins = []
-
         yPosition = screenHeight - 150
 
         for x in range(numBins):
-            dcenter = abs(x - (numBins - 1)/2)
-            gradientRatio = dcenter / ((numBins - 1) / 2)
+           
+            distCenter = floor(abs(x - (numBins - 1) / 2))
+            gradientRatio = distCenter / ((numBins - 1) / 2)
             red = int(255 * gradientRatio)
-            yellow = int(255 *(1-gradientRatio))
-            color = (red,yellow,0)
-            self.bins.append(Bin((screenLength / 2 - spacing * numBins / 2 - binWidth * numBins / 2 + (spacing + binWidth) * x, yPosition), color))
+            yellow = int(255 * (1 - gradientRatio))
+            color = (red, yellow, 0)
 
+            distCenterDict = {0: 0.3, 1: 0.5, 2: 1, 3: 2, 4: 5, 5: 10}
+
+            if distCenter in distCenterDict:
+                multiplier = distCenterDict[distCenter]
+            else:
+                multiplier = 50
+            
+            bin_x_position = (
+                screenLength / 2
+                - spacing * numBins / 2
+                - binWidth * numBins / 2
+                + (spacing + binWidth) * x
+            )
+            self.bins.append(Bin((bin_x_position, yPosition), color, multiplier))
+    
     def createPegs(self):
         self.pegs = []
             
         for y in range(20):
             for x in range(y + 1):
-                
-                # Gradient color based on position
+                if not y:
+                    continue
+
 
                 maxPeg = 20 // 2 + abs(20 // 2 - 20)
                 yColorDiff = y // 2
@@ -98,9 +141,12 @@ class Board:
                 self.pegs.append(Peg((screenLength / 2 + pegSpacingX * (x - y / 2), 100 + y * pegSpacingY), (red, blue, green)))
 
     def addBall(self, position):
-        self.balls.append(Ball(pygame.mouse.get_pos()))
+        if self.clickableArea.intersectMouse():
+            self.balls.append(Ball(pygame.mouse.get_pos()))
 
     def draw(self):
+        self.clickableArea.draw()
+
         for bin in self.bins:
             bin.draw()
 
@@ -122,14 +168,12 @@ while True:
             if event.button == 1:
                 board.addBall(pygame.mouse.get_pos())
 
+        if event.type == pygame.MOUSEMOTION:
+            if pygame.mouse.get_pressed()[0]:  # Left mouse button is held down
+                board.addBall(pygame.mouse.get_pos())
+
     screen.fill((0, 0, 0)) 
     board.draw()
     
     pygame.display.update()
     clock.tick(60)
-
-
-
-
-
-
